dcffdb3028544e8b1aba499699f11a0efed87d68
Mohammed <dev.mohammedsh@gmail.com>
2024-10-08 17:59:27 +0530

    🗄️ Database connected

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 
+import { connectDB } from "./db/connectDB.js"
+
 // routes imports
 import authRoutes from "./routes/auth.routes.js"
 
@@ -14,5 +16,6 @@ const PORT = process.env.PORT || 5000
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
+    connectDB()
 	console.log(`Server is running at http://localhost:${PORT}`)
 })
