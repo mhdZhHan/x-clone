3de99cd36d04e1040b79876e9e5ebc2518b311e7
Mohammed <dev.mohammedsh@gmail.com>
2024-10-11 22:38:09 +0530

    ✅ All other pages and components (Done UI)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
 import { Routes, Route } from "react-router-dom"
 
+import Sidebar from "./components/common/Sidebar"
+
 // pages
 import HomePage from "./pages/home/HomePage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
 import LoginPage from "./pages/auth/login/LoginPage"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
 
 function App() {
 	return (
 		<div className="flex max-w-6xl mx-auto">
+			<Sidebar />
+
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/signup" element={<SignUpPage />} />
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/notifications" element={<NotificationPage />} />
+				<Route path="/profile/:username" element={<ProfilePage />} />
 			</Routes>
 		</div>
 	)
