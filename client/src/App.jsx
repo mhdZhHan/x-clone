71dad7c67e489fedc27124574101819ba981a7f6
Mohammed <dev.mohammedsh@gmail.com>
2024-10-11 19:39:48 +0530

    ⚛️ React setup

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+
+// pages
+import HomePage from "./pages/home/HomePage"
+import SignUpPage from "./pages/auth/signup/SignUpPage"
+import LoginPage from "./pages/auth/login/LoginPage"
+
+function App() {
+	return (
+		<div className="flex max-w-6xl mx-auto">
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/login" element={<LoginPage />} />
+			</Routes>
+		</div>
+	)
+}
+
+export default App
